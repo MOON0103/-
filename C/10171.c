867c531c4778d1c04a021a588d4d426e3b6df502
MOON0103 <80956983+MOON0103@users.noreply.github.com>
2021-10-20 20:22:20 +0900

    Create 10171.c

---
+++ C/10171.c
@@ -0,0 +1,22 @@
+1
+#include <stdio.h> 
+2
+​
+3
+int main()
+4
+{   
+5
+  printf("\\    /\\\n");
+6
+  printf(" )  ( ')\n");
+7
+  printf("(  /  )\n");
+8
+  printf(" \\(__)|\n");
+9
+​
+10
+  return 0;
+11
+}
