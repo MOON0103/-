d1d521ac17c0bf8c4fcdf4c1ef92200c07caeda9
MOON0103 <80956983+MOON0103@users.noreply.github.com>
2021-10-20 20:23:30 +0900

    Create 10430.c

---
+++ C/10430.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, res;
+    scanf("%d %d %d", &a, &b, &c);
+
+    res = (a + b) % c;
+    printf("%d\n", res);
+
+    res = ((a % c) + (b % c)) % c;
+    printf("%d\n", res);
+
+    res = (a * b) % c;
+    printf("%d\n", res);
+
+    res = ((a % c) * (b % c)) % c;
+    printf("%d\n", res);
+    
+    return 0;
+}
