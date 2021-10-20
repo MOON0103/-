0bd031831701be96c386f4529bd5a5a4c89f66a8
MOON0103 <80956983+MOON0103@users.noreply.github.com>
2021-10-20 20:21:22 +0900

    Create 8393.c

---
+++ C/8393.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+  int a, i, b;
+
+  b = 0;
+
+  scanf("%d", &a);
+
+  
+
+  for(int i = 1; i <=  a; i++)
+
+  {
+
+     b = b + i;    
+
+  }
+
+  
+
+  printf("%d", b);
+
+  return 0;
+
+}
