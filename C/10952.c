0f8c0de61c04c2ba82466420bdd2b9d797710a24
MOON0103 <80956983+MOON0103@users.noreply.github.com>
2021-10-20 21:06:10 +0900

    Create 10952.c

---
+++ C/10952.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 1, b = 1;
+
+	while (a != 0 && b != 0)
+	{
+		scanf("%d %d", &a, &b);
+    if(a == 0 && b == 0) break;
+		printf("%d\n", a + b);
+
+	}
+	return 0;
+}
